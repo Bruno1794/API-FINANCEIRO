8a593070a7902c022fc1838a19b2dfb264d09051
Bruno <brunocostasilva62@gmail.com>
2025-01-17 11:46:10 -0300

    criado crud contas a pagar.

--- database/migrations/2025_01_10_165910_create_debts_table.php
+++ database/migrations/2025_01_10_165910_create_debts_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('debts', function (Blueprint $table) {
             $table->id();
+            $table->decimal('value_debit');
+            $table->decimal('value_discount')->nullable();
+            $table->string('parcel')->nullable();
+            $table->enum('type_payment', ['DEBITO','CREDITO', 'DINHEIRO','PIX'])->default('DEBITO');
+            $table->enum('status_debit', ['PENDING', 'PAID'])->nullable()->default('PENDING');
+            $table->date('date_paid')->nullable();// data de pagamento
+            $table->date('date_maturity')->nullable();//data de vencimento
+            $table->string('observation')->nullable();
+            $table->foreignId('banck_id')->nullable()->constrained('banks');
+            $table->foreignId('provider_id')->nullable()->constrained('providers');
+            $table->foreignId('card_id')->nullable()->constrained('cards');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
