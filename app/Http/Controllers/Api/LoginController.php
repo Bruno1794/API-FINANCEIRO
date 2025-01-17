9412f1741a99e4da3336e3af98e3acae9a79b15f
Bruno <brunocostasilva62@gmail.com>
2025-01-17 11:57:02 -0300

    Revert "criado crud contas a pagar."
    This reverts commit 95671184b420c6ca3630557b9bc087d842ddcb4b.

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -29,41 +28,6 @@ class LoginController extends Controller
                 'message' => 'Usuario ou Senha Incorretos!'
             ]);
         }
-
-    }
-
-    public function logout(): JsonResponse
-    {
-        $userLogado = Auth::check();
-
-        if ($userLogado) {
-            $user = User::where('id', Auth::id())->first();
-            $user->tokens()->delete();
-            return response()->json([
-                'success' => true,
-                'message' => 'Logout realizado com sucesso!'
-            ], 200);
-        } else {
-            return response()->json([
-                'success' => false,
-                'message' => 'Falha ao fazer logout!'
-            ]);
-        }
-
-    }
-    public function validaToken(): JsonResponse
-    {
-        try {
-            return response()->json([
-                'success' => true,
-            ]);
-        }catch (\Exception $exception){
-            return response()->json([
-                'success' => false,
-            ]);
-        }
-
-
 
     }
 }
